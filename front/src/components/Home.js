fe972222528a132adb94b73abdb198215fbd17d6
Keval <keval9464@gmail.com>
2023-05-11 11:45:44 +0530

    initial commit

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 
 const Home = () => {
     const [data, setData] = useState([])
+    const [err, setErr] = useState([])
+
+
+    const getAlldata = async () => {
+
+        try {
+            const getData = await axios.get('http://localhost:5000/getdata')
+            setData(getData.data.data)
+            // console.log(getData.data.data);
+        } catch (error) {
+            setErr(error.message)
+        }
+
+    }
 
     useEffect(() => {
-        fetch('http://localhost:5000/getdata')
-            .then(res => res.json())
-            .then((data) => {
-                setData(data.data)
-            })
+        // fetch('http://localhost:5000/getdata')
+        //     .then(res => res.json())
+        //     .then((data) => {
+        //         setData(data.data)
+        //     })
+
+        getAlldata();
+
     }, [])
 
     return (
@@ -18,22 +36,26 @@ const Home = () => {
             <div className='auth-wrapper mt-5'>
                 <h3 className='mt-3' >Welcome</h3>
                 <div className='mt-8'>
+                    <div>
+                        {err !== "" && <div> {err}</div>}
+                    </div>
 
                     <table style={{ width: '100%', border: "1px solid black" }}  >
                         <tbody>
                             <tr >
                                 <th style={{ border: "1px solid black" }}>Name</th>
                                 <th style={{ border: "1px solid black" }}>email</th>
-                                <th style={{ border: "1px solid black" }}>password</th>
                             </tr>
 
 
-                            {data.map((i, key) => {
+                            {data.map((i) => {
+
+                                const { _id, email, name } = i
+
                                 return (
-                                    <tr key={key} >
-                                        <td style={{ border: "1px solid black" }}> {i.name} </td>
-                                        <td style={{ border: "1px solid black" }}>{i.email} </td>
-                                        <td style={{ border: "1px solid black" }}>{i.password} </td>
+                                    <tr key={_id} >
+                                        <td style={{ border: "1px solid black" }}> {name} </td>
+                                        <td style={{ border: "1px solid black" }}>{email} </td>
                                     </tr>
                                 )
                             })}
